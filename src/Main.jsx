f363e535fac1f75fd1c3e2f295068016d78d709b
Cem Ergin <birazgecemuzigi@gmail.com>
2021-01-23 10:09:40 -0500

    eslint + prettier setup

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,17 +1,16 @@
-import React from "react";
-import { Provider } from "react-redux";
+import React from 'react'
 
-import App from "./containers/App/App";
-import configureStore from "./configureStore";
-
-const store = configureStore();
+import { Provider } from 'react-redux'
+import App from './containers/App/App'
+import configureStore from './configureStore'
+const store = configureStore()
 
 const Main = () => {
   return (
     <Provider store={store}>
       <App />
     </Provider>
-  );
-};
+  )
+}
 
-export default Main;
+export default Main
