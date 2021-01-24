65231e1041250c3e05aa4c11079e3b56f595ed6e
Mikee Siopongco <asiopongco@gmail.com>
2021-01-24 17:14:24 -0500

    Add Sessions Dashboard

--- src/components/sessions/SessionListItem.js
+++ src/components/sessions/SessionListItem.js
@@ -1,21 +1,26 @@
-/* eslint-disable prettier/prettier */
-import React from 'react'
-import { List } from 'semantic-ui-react'
+import React from "react";
+import { Icon, List } from "semantic-ui-react";
 
-const sessionInfo = {
-  book: 'The Cat in the Hat',
-  student: 'Antonio',
-  user: 'Michael',
-  notes: 'This is a note',
-  dateOfSession: new Date(),
-}
-
-const SessionListItem = () => {
+const SessionListItem = ({ session }) => {
   return (
     <List.Item>
-      <List.Content>{sessionInfo.book}</List.Content>
+      <List.Content verticalAlign="middle" floated="left">
+        {session.index}
+      </List.Content>
+      <List.Content verticalAlign="middle">
+        <div className="middle aligned">
+          {session.book}
+          {session.dateOfSession}
+        </div>
+      </List.Content>
+      <List.Content floated="right" verticalAlign="middle">
+        <div>
+          <Icon name="edit" />
+          <Icon name="trash alternate outline" />
+        </div>
+      </List.Content>
     </List.Item>
-  )
-}
+  );
+};
 
-export default SessionListItem
+export default SessionListItem;
