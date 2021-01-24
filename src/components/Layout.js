bf82c011ed6faaffb45411a7c16c2efabb0a5673
Steven Balasta <steven.balasta@unqork.com>
2021-01-24 15:55:27 -0500

    adds sessions page header component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import Header from './Header'
-import Footer from './Footer'
-import './Layout.css'
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import "./Layout.css";
 
 const Layout = ({ children, navbar = true, footer = true, white = true }) => {
   return (
     <div className="Layout">
       {navbar ? <Header className="Navbar"></Header> : null}
-      <main className={`Main ${white ? 'white' : ''}`}>{children}</main>
+      <main className={`Main ${white ? "white" : ""}`}>{children}</main>
       {footer ? <Footer className="Footer"></Footer> : null}
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
