e857dbff1e37af840bc3fbc8f0e4cadada630125
Peter Choi <jc4883@columbia.edu>
2021-01-24 15:25:13 -0500

    implement header on every page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import Logo from '../assets/logo.png'
+
+import './Header.css'
 
 const Header = () => {
+  const username = "Peter"
   return (
-    <>
-      <header>Header</header>
-    </>
+      <header className="header">
+        <img className="header-logo" src={Logo} alt=""/>
+        <div className="profile">
+          <span className="username">{username}</span>
+          <div className="profile-icon"/>
+        </div>
+      </header>
   )
 }
 
