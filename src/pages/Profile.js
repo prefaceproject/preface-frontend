3b81a23d73b4e2f47f175add9115dd131b69d1f1
git checkout -b sisqrd <sisiyu.design@gmail.com>
2021-01-24 18:35:22 -0500

    profilechanges

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,26 +2,34 @@ import React from "react";
 import Layout from "../components/Layout";
 import { Form } from "semantic-ui-react";
 import "../index.css";
+import SessionsPageHeader from "../components/SessionsPageHeader";
 
 const Profile = () => {
   return (
     <>
-      <Layout navbar={false}>
-        <h2>User Profile</h2>
-        <Form>
-          <Form.Field>
-            <label>First Name</label>
-            <input placeholder="First Name" />
-          </Form.Field>
-          <Form.Field>
-            <label>Last Name</label>
-            <input placeholder="Last Name" />
-          </Form.Field>
-          <Form.Field>
-            <label>Email</label>
-            <input placeholder="Email" />
-          </Form.Field>
-        </Form>
+      <Layout>
+        <SessionsPageHeader></SessionsPageHeader>
+        <div style={{ width: "50%" }}>
+          <h1>User Profile</h1>
+          <Form>
+            <Form.Field>
+              <label>First Name</label>
+              <input placeholder="First Name" />
+            </Form.Field>
+            <Form.Field>
+              <label>Last Name</label>
+              <input placeholder="Last Name" />
+            </Form.Field>
+            <Form.Field>
+              <label>Email</label>
+              <input placeholder="Email" />
+            </Form.Field>
+            <Form.Field>
+              <label>Languages Spoken</label>
+              <input placeholder="Languages Spoken" />
+            </Form.Field>
+          </Form>
+        </div>
       </Layout>
     </>
   );
