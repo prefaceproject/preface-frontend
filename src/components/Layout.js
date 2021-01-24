985fe9531f8c0058c08adb41afe95d3c3ed4c3ec
Cem Ergin <birazgecemuzigi@gmail.com>
2021-01-23 20:19:15 -0500

    Layout Updates

---
+++ src/components/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+const Layout = ({ children }) => {
+  ;<>
+    <Header></Header>
+    <main>{childred}</main>
+    <Footer></Footer>
+  </>
+}
+
+export default Layout
