98d18c82fa866a0cbaab6cb7b3c6a2d9c36b256f
Francis Kigawa <fkigawa@Franciss-MacBook-Pro.local>
2021-01-27 00:18:54 -0500

    create user redux/sagas

--- src/store/user/selectors.js
+++ src/store/user/selectors.js
@@ -1,1 +1,4 @@
 export const getUser = ({ user: { data } }) => data;
+export const getAllAmbassadors = ({user: { ambassadorList } }) => ambassadorList;
+export const getAllTeachers = ({user: { teacherList } }) => teacherList;
+export const getAllStudents = ({user: { studentList } }) => studentList;
