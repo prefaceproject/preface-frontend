14f15f8274688f023d7c265529a919eda86baac5
Kofi <w.fifi99@gmail.com>
2021-03-02 23:08:40 -0500

    user update functionality

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import Layout from "../components/Layout";
-import { Form, Button, Container } from "semantic-ui-react";
+import { Form, Button, Container, Dropdown } from "semantic-ui-react";
 import "../index.css";
 import { connect } from "react-redux";
 import ProfilePageHeader from "../components/ProfilePageHeader";
 import { Link } from "react-router-dom";
-import "./styles/Profile.css";
 
 import "./styles/Profile.css";
 import ChangePasswordModal from "../components/Modals/ChangePasswordModal";
+import {useDispatch, useSelector} from 'react-redux';
+import * as userActions from "../store/user/actions";
+
+
 
 const Profile = ({ user }) => {
   console.log(user);
@@ -17,10 +20,70 @@ const Profile = ({ user }) => {
   const [email, setEmail] = useState(user.email);
   const [modalOpen, setModalOpen] = useState(false);
   const [passwordStatus, setPasswordStatus] = useState({success: false, message: ""});
+  const [school, setSchool] = useState(user.school);
+  const [languagesSpoken, setLanguagesSpoken] = useState(user.languagesSpoken);
+  const [isActive, setIsActive] = useState(user.isActive);
+  const [students, setStudents] = useState(user.students);
+  const dispatch = useDispatch();
+
+
+  const languagesSpokenOptions = [
+    { key: 'English', value: 'English', text: 'English' },
+    { key: 'French', value: 'French', text: 'French' },
+    { key: 'Spanish', value: 'Spanish', text: 'Spanish' },
+    { key: 'German', value: 'German', text: 'German' },
+    { key: 'Italian', value: 'Italian', text: 'Italian' },
+    { key: 'Greek', value: 'Greek', text: 'Greek' },
+    { key: 'Mandarin', value: 'Mandarin', text: 'Mandarin' },
+    { key: 'Japanese', value: 'Japanese', text: 'Japanese' }
+    ]
+
 
   const closeModal = () => {
     setModalOpen(false);
   };
+
+  function handleSave() {
+    if (user.role == 'ambassador') {
+      dispatch(userActions.updateAmbassador({ 
+        user: {
+            role: "ambassador", 
+            firstName: firstName,
+            lastName: lastName,
+            email: email, 
+            students: students,
+            isActive: isActive,
+            _id: user._id,
+            school: school,
+            languagesSpoken: languagesSpoken
+        } 
+    }))       
+
+    }
+    else {
+      console.log(user.school)
+      dispatch(userActions.updateTeacher({ 
+        user: {
+            role: "teacher", 
+            firstName: firstName,
+            lastName: lastName,
+            email: email, 
+            students: students,
+            isActive: isActive,
+            _id: user._id,
+            school: school,
+            languagesSpoken: languagesSpoken
+        } 
+    }))        
+    }
+}
+
+const handleLanguagesSpoken = (e, {value}) => {
+  console.log("WUNNA")
+  setLanguagesSpoken(value)
+}
+
+
 
   return (
     <>
@@ -44,11 +107,11 @@ const Profile = ({ user }) => {
               </Form.Field>
               <Form.Field>
                 <label>Languages Spoken</label>
-                <input placeholder="Languages Spoken" readOnly />
-              </Form.Field>
+                <Dropdown value={languagesSpoken} onChange={handleLanguagesSpoken.bind(this)} fluid multiple selection options={languagesSpokenOptions} />
+            </Form.Field>
               <Form.Field>
                 <label>School</label>
-                <input placeholder="Add School Name" />
+                <input placeholder="Add School Name" value = {school}  onChange={(event) =>  setSchool(event.target.value)}/>
               </Form.Field>
               <div className="buttons">
                 <Button 
@@ -57,7 +120,7 @@ const Profile = ({ user }) => {
                   onClick={() => setModalOpen(true)}>
                     Change Password
                 </Button>
-                <Button color="blue">Save</Button>
+                <Button color="blue" onClick={handleSave}>Save</Button>
               </div>
               <div className={passwordStatus.success ? "passwordStatus pass" : "passwordStatus fail"}>
                 {passwordStatus.message}
