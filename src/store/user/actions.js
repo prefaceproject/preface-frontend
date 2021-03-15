c24be7ec4d7608346b67a4e22523b4a048fd83d6
Francis Kigawa <fkigawa@Franciss-MacBook-Pro.local>
2021-03-15 00:13:14 -0400

    finish profile page functionality

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -45,3 +45,9 @@ export const setAutoLoginError = (err) =>
   action(actionTypes.SET_AUTO_LOGIN_ERROR, err);
 
 export const fetchUser = (data) => action(actionTypes.FETCH_USER, data);
+
+export const updateAmbassadorProfile = (data) =>
+  action(actionTypes.UPDATE_AMBASSADOR_PROFILE, data);
+
+export const updateTeacherProfile = (data) =>
+  action(actionTypes.UPDATE_TEACHER_PROFILE, data);
